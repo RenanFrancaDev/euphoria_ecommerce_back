29cc546915431c4e71710f309a7654aeec90bf11
Renan Franca <renanfranca.dev@gmail.com>
2024-02-08 09:08:15 -0300

    feat: auth system working, ensure that you need do auth with login to register your infos

---
+++ src/app/routes/auth.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import { login, register } from "../controllers/auth.js";
+
+// import { authMiddleware } from "../../middleware/authMiddleware.js";
+
+const router = Router();
+
+router.post("/login", async (req, res) => {
+  try {
+    const data = await login(req.body);
+    res.status(200).json({ data });
+  } catch {
+    res.status(500).json(error);
+  }
+});
+
+//Package express bearer token to send req.token
+
+router.post("/register", async (req, res) => {
+  try {
+    const data = await register(req.body);
+    res.status(200).json({ data });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+export default router;
