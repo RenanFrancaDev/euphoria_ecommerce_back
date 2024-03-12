42cc156ba213db398b3dd3b348309b76d26467c5
Renan Franca <renanfranca.dev@gmail.com>
2024-03-11 21:16:30 -0300

    feat: order controller created

--- src/app/controllers/orders.js
+++ src/app/controllers/orders.js
@@ -15,12 +15,31 @@ export const getByOrder = (order_id) => {
 };
 
 export const getByUser = (user_id) => {
-  console.log("oi");
   return knex(TABLE).where({ user_id });
 };
 
-export const save = (params) => {
-  return knex(TABLE).insert(params);
+export const save = async (params) => {
+  console.log(params);
+  const order = await knex(TABLE).insert({
+    user_id: params.user_id,
+    total_price: params.total_price,
+    total_discount: params.total_discount,
+    total_price_current: params.total_price_current,
+    status: params.status,
+  });
+
+  for (const product of params.products) {
+    await knex("order_product").insert({
+      user_id: params.user_id,
+      order_id: order[0],
+      product_id: product.id,
+      price: product.price,
+      discount: product.discount,
+      current_price: product.current_price,
+      quantity: product.quantity,
+    });
+  }
+  return order;
 };
 
 export const update = (id, params) => {
